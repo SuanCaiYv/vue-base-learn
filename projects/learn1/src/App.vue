e1f1aaf65ac58e47eeaeb829208d59dcfcc062b9
SuanCaiYv <jokerblack2508826394@gmail.com>
2021-12-03 18:07:51 +0800

    learn1

--- projects/learn1/src/App.vue
+++ projects/learn1/src/App.vue
@@ -1,15 +1,31 @@
 <script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import MyBook from "./components/MyBook.vue";
+import { ref } from "vue";
+
+const child = ref(null);
+
+const childIncrement = function () {
+  child.value.increment()
+};
 </script>
 
 <template>
+  <div>
+    <MyBook ref="child"></MyBook>
+    <button @click="childIncrement">+1</button>
+  </div>
 </template>
 
 <style>
 #app {
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
--- projects/learn1/src/components/MyBook.vue
+++ projects/learn1/src/components/MyBook.vue
@@ -1,8 +1,22 @@
 <script setup>
+import {ref} from 'vue'
+
+const counter = ref(0)
+
+const increment = function() {
+    counter.value++
+}
+
+const msg = "aaa"
+
+defineExpose({
+    increment,
+    msg
+})
 </script>
 
 <template>
+<p>
+    {{counter}}
+</p>
 </template>
-
-<style>
-</style>
