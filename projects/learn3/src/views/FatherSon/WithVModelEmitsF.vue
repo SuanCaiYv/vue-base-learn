16c0dde488b987d9b0226fd848d3df28457af680
SuanCaiYv <jokerblack2508826394@gmail.com>
2021-12-07 14:20:28 +0800

    父子间组件通信——v-model-emits

---
+++ projects/learn3/src/views/FatherSon/WithVModelEmitsF.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <WithVModelEmitsS v-model:age="myAge" v-model:msg="name" v-model:time-str="timeString"></WithVModelEmitsS>
+        <br>
+        {{ timeString }}
+    </div>
+</template>
+
+<script lang="ts" setup>
+import {ref} from "vue"
+import WithVModelEmitsS from "./WithVModelEmitsS.vue";
+
+const name = ref<String>("WithVModelEmitsF")
+const myAge = ref<Number>(21)
+
+// 此变量和子组件的timeStr属性完成绑定，所以会被同步更新
+const timeString = ref<String>("")
+</script>
+
+<style scoped>
+
+</style>
---
+++ projects/learn3/src/views/FatherSon/WithVModelEmitsS.vue
@@ -0,0 +1,33 @@
+<template>
+    <div></div>
+</template>
+
+<script lang="ts" setup>
+import {ref} from "vue"
+
+const name = ref<String>("WithVModelEmitsS")
+
+const props = defineProps({
+    msg: String,
+    age: Number,
+    timeStr: String
+})
+
+console.log(props.msg + " + " + props.age)
+
+const emits = defineEmits([
+    // v-model版本的父子通信只能触发update类型的事件
+    "update:msg",
+    "update:age",
+    "update:timeStr"
+])
+
+setInterval(() => {
+    // 只能触发已定义的属性的update事件，且如果想通知父组件，必须事先把父组件变量和子组件属性完成绑定
+    emits("update:timeStr", new Date().toTimeString())
+}, 1000)
+</script>
+
+<style scoped>
+
+</style>
