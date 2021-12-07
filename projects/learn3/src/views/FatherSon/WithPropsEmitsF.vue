4ff3daee718444a7ef56f903208e6c39bd14d84b
SuanCaiYv <jokerblack2508826394@gmail.com>
2021-12-07 14:20:10 +0800

    父子间组件通信——props-emits

---
+++ projects/learn3/src/views/FatherSon/WithPropsEmitsF.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <!--@子组件事件名="<父组件函数>的形式可以接收子组件发出的事件并处理"-->
+        <!--v-bind把父组件的变量的值绑定到子组件属性上-->
+        <WithPropsEmitsS :age="21" :msg="name" @showTime="timeStr"></WithPropsEmitsS>
+        <br>
+        {{ time }}
+    </div>
+</template>
+
+<script lang="ts" setup>
+import {ref} from "vue"
+import WithPropsEmitsS from "./WithPropsEmitsS.vue"
+
+const name = ref<String>("WithPropsEmitsF")
+const time = ref<String>("")
+
+const timeStr = function (val: String): void {
+    time.value = val
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ projects/learn3/src/views/FatherSon/WithPropsEmitsS.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        {{ msg }}
+        <br>
+        {{ age }}
+    </div>
+</template>
+
+<script lang="ts" setup>
+import {ref} from "vue"
+
+const name = ref<String>("WithPropsEmitsS")
+
+// 定义属性，用来接收来自父组件的值
+const props = defineProps({
+    msg: String,
+    age: Number
+})
+
+console.log(props.msg + " + " + props.age)
+
+// 子组件定义要发出的事件
+const emits = defineEmits([
+    "showTime"
+])
+
+setInterval(() => {
+    // 发出事件，并设置事件参数，由父组件接收
+    emits("showTime", new Date().toTimeString())
+}, 1000)
+</script>
+
+<style scoped>
+
+</style>
